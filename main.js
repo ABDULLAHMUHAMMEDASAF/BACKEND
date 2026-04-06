6b4272d6ec5664fa1c5cad977fd2715ff3ea6188
ABDULLAHMUHAMMEDASAF <emrecanemre49@gmail.com>
2026-04-06 06:57:30 +0300

    Updated 06:57

--- main.js
+++ main.js
@@ -1,70 +1,30 @@
-const mongoose = require("mongoose");
-const User = require("./models/User");
-const MongoUser = require("./models/MongoUser");
-const cors = require("cors");
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 1453;
-require("dotenv").config();
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnHakkimda");
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB bağlantısı başarılı"))
-  .catch((err) => console.log("MongoDB bağlantı hatası:", err));
+  const bekle = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-//! JSON parse middleware
-app.use(express.json());
-app.use(cors());
+  btn.addEventListener("click", async () => {
+    let noktaSayisi = 0;
+    const interval = setInterval(() => {
+      noktaSayisi = (noktaSayisi % 3) + 1;
+      console.clear();
+      console.log("Bekliyor" + ".".repeat(noktaSayisi));
+    }, 500);
 
-//! GET endpoint
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+    await bekle(2000);
+    clearInterval(interval);
+    console.clear();
+    console.log("Merhaba async");
 
-//! POST endpoint
-app.post("/users", async (req, res) => {
-  const newUser = new User(req.body);
-  await newUser.save();
-  res.status(201).json(newUser);
-});
-
-//! DELETE endpoint
-app.delete("/users/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const deletedUser = await User.findByIdAndDelete(id);
-
-  if (!deletedUser) {
-    return res.status(404).json({ message: "User bulunamadı" });
-  }
-
-  res.json({ message: "User silindi", user: deletedUser });
-});
-
-//! UPDATE endpoint
-app.put("/users/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
-
-  if (!updatedUser) {
-    return res.status(404).json({ message: "User bulunamadı" });
-  }
-
-  res.json(updatedUser);
-});
-
-//! ---------------------------------- MONGO USERS ------------------------------------------
-app.get("/mongo-users", async (req, res) => {
-  const users = await MongoUser.find();
-  res.json(users);
-});
-
-app.get("/asaf", (req, res) => {
-  res.send("Merhaba zalim backend dünyası");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server ayağa kalktı ve ${PORT} portunu dinliyor...`);
+    const res = await fetch("http://localhost:1453/hakkimda");
+    const data = await res.json();
+    const ul = document.createElement("ul");
+    console.log(data.length);
+    data.forEach((el) => {
+      const li = document.createElement("li");
+      li.textContent = el.id + " " + el.name + " " + el.age;
+      ul.appendChild(li);
+    });
+    btn.appendChild(ul);
+  });
 });
---
+++ server.js
@@ -0,0 +1,99 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const MongoUser = require("./models/MongoUser");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB bağlantısı başarılı"))
+  .catch((err) => console.log("MongoDB bağlantı hatası:", err));
+
+//! JSON parse middleware
+app.use(express.json());
+app.use(cors());
+
+//! GET endpoint
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+//! GET endpoint
+app.get("/hakkimda", (req, res) => {
+  res.json([
+    { id: 1, name: "Emre", age: 40 },
+    { id: 2, name: "Meliha", age: 35 },
+  ]);
+});
+
+//! POST end point
+app.post("/kullanicilar", (req, res) => {
+  const yeniKullanici = req.body;
+  console.log(yeniKullanici);
+  res.json({
+    mesaj: "Kullanıcı oluşturuldu",
+    kullanici: yeniKullanici,
+  });
+});
+
+//! PUT end point
+app.put("/kullanicilar/:id", (req, res) => {
+  const id = req.params.id;
+  const guncellenenVeri = req.body;
+
+  res.json({
+    mesaj: `${id} numaralı kullanıcı güncellendi.`,
+    yeniVeri: guncellenenVeri,
+  });
+});
+
+//! POST endpoint
+app.post("/users", async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.status(201).json(newUser);
+});
+
+//! DELETE endpoint
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const deletedUser = await User.findByIdAndDelete(id);
+
+  if (!deletedUser) {
+    return res.status(404).json({ message: "User bulunamadı" });
+  }
+
+  res.json({ message: "User silindi", user: deletedUser });
+});
+
+//! UPDATE endpoint
+app.put("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+
+  if (!updatedUser) {
+    return res.status(404).json({ message: "User bulunamadı" });
+  }
+
+  res.json(updatedUser);
+});
+
+//! ---------------------------------- MONGO USERS ------------------------------------------
+app.get("/mongo-users", async (req, res) => {
+  const users = await MongoUser.find();
+  res.json(users);
+});
+
+app.get("/asaf", (req, res) => {
+  res.send("Merhaba zalim backend dünyası");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ayağa kalktı ve ${PORT} portunu dinliyor...`);
+});
